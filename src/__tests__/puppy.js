cf600cbfa6f7aaf076c894635932d962b2619c24
jjohns95 <j.johnson55520@yahoo.com>
2022-03-21 10:09:11 -0400

    Add the puppy pojo

---
+++ src/__tests__/puppy.js
@@ -0,0 +1,18 @@
+
+
+class Puppy {
+    constructor(name, breed){
+      this.name = name
+      this.breed = breed
+    }
+  
+    speak() {
+      console.log(`${name} says 'arf'`)
+    }
+  
+    about() {
+      console.log(`${name} is just a lil pup. He's a ${breed}`)
+    }
+  }
+  
+  module.exports = Puppy
